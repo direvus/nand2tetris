a4f2435fc35cf6c0f21d40cbf24b268cd96b0be0
Brendan Jurd <brendan.jurd@novasystems.com>
2025-02-13 15:02:26 +1100

    Add more predefined symbols

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -58,6 +58,11 @@ SYMBOLS = {
         'R15': 15,
         'SCREEN': 16384,
         'KBD': 24576,
+        'SP': 0,
+        'LCL': 1,
+        'ARG': 2,
+        'THIS': 3,
+        'THAT': 4,
         }
 
 
