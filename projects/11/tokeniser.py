d7884b92f31375ef030908f8148cf3f788210f17
Brendan Jurd <brendan.jurd@novasystems.com>
2025-03-05 16:06:08 +1100

    Update tokeniser for project 11.
    I decided to upgrade the tokeniser for this project, to capture the line
    and column position of where each token was found in the source code.
    This will make it a lot easier to debug problems with the compiler (or
    with source code on which the compiler is operating).

--- projects/11/tokeniser.py
+++ projects/11/tokeniser.py
@@ -10,12 +10,30 @@ KEYWORDS = {
         'int', 'char', 'boolean', 'void',
         'true', 'false', 'null', 'this',
         'let', 'do', 'if', 'else', 'while', 'return'}
-ESCAPES = (
-        ('&', '&amp;'),
-        ('<', '&lt;'),
-        ('>', '&gt;'),
-        ('"', '&quot;'),
-        )
+
+
+def html_escape(text):
+    return text.replace(
+            '&', '&amp;').replace(
+            '<', '&lt;').replace(
+            '>', '&gt;').replace(
+            '"', '&quot;')
+
+
+class Token:
+    def __init__(self, token_type, value, line, position):
+        self.token_type = token_type
+        self.value = value
+        self.line = line
+        self.position = position
+
+    def __str__(self):
+        return f"{self.token_type} {self.value} @ {self.line}:{self.position}"
+
+    def to_xml(self):
+        tag = self.token_type
+        content = html_escape(self.value)
+        return f'<{tag}> {content} </{tag}>'
 
 
 class Tokeniser:
@@ -25,16 +43,17 @@ class Tokeniser:
     source code file, it can be used to emit a stream of valid code tokens in
     the file. You can do this by checking the return of the has_next() method,
     and then calling advance() if has_next() was True. After calling advance(),
-    the Tokeniser will have a token stored in its `token_type` and `value`
-    attributes. Or, you can simply loop over the `generate()` method which will
-    take care of all this for you.
+    the Tokeniser will have a token stored in its `token` attribute which you
+    can retrieve. Or, you can simply loop over the `generate()` method which
+    will take care of all this for you.
     """
     def __init__(self, stream):
         self.stream = stream
         self.buf = self.stream.read()
+        self.line = 0
+        self.position = 0
         self.index = 0
-        self.token_type = None
-        self.value = None
+        self.token = None
 
     def has_next(self):
         """Return whether there are any more tokens in the input stream.
@@ -48,7 +67,11 @@ class Tokeniser:
         try:
             while ch.isspace() or ch2 in {'//', '/*'}:
                 while self.buf[i].isspace():
+                    if self.buf[i] == '\n':
+                        self.line += 1
+                        self.position = 0
                     i += 1
+                    self.position += 1
 
                 ch2 = self.buf[i: i+2]
                 if ch2 == '//':
@@ -56,13 +79,19 @@ class Tokeniser:
                     while self.buf[i] != '\n':
                         i += 1
                     i + 1
-                    ch = self.buf[i]
+                    self.line += 1
+                    self.position = 1
 
                 elif ch2 == '/*':
                     # Skip characters until end-of-comment marker
                     while self.buf[i: i+2] != '*/':
+                        if self.buf[i] == '\n':
+                            self.line += 1
+                            self.position = 0
                         i += 1
+                        self.position += 1
                     i += 2
+                    self.position += 2
                 ch = self.buf[i]
                 ch2 = self.buf[i: i+2]
 
@@ -74,42 +103,50 @@ class Tokeniser:
         except IndexError:
             return False
 
+    def set_token(self, token_type, value):
+        self.token = Token(token_type, value, self.line, self.position)
+        return self.token
+
     def advance(self):
         """Consume one token from the input.
 
-        Set the `token_type` and `value` attributes to the next token in the
-        input stream, and move the internal pointer to the next character
-        following the end of the token.
+        Set the `token` attribute to the next token in the input stream, and
+        move the internal pointer to the next character following the end of
+        the token.
         """
         i = self.index
         ch = self.buf[i]
         if ch in SYMBOLS:
-            self.token_type = 'symbol'
-            self.value = ch
+            self.set_token('symbol', ch)
             self.index += 1
+            self.position += 1
         elif ch.isdigit():
             i += 1
             while self.buf[i].isdigit():
                 i += 1
-            self.token_type = 'integerConstant'
-            self.value = int(self.buf[self.index: i])
+            value = int(self.buf[self.index: i])
+            self.set_token('integerConstant', value)
+            self.position += (i - self.index)
             self.index = i
         elif ch == '"':
             i += 1
             while self.buf[i] != '"':
                 i += 1
-            self.token_type = 'stringConstant'
-            self.value = self.buf[self.index + 1: i]
+            value = self.buf[self.index + 1: i]
+            self.set_token('stringConstant', value)
+            self.position += (i - self.index)
             self.index = i + 1
         elif ch.isalpha() or ch == '_':
             i += 1
             while self.buf[i].isalnum() or self.buf[i] == '_':
                 i += 1
-            self.value = self.buf[self.index: i]
-            if self.value in KEYWORDS:
-                self.token_type = 'keyword'
+            value = self.buf[self.index: i]
+            if value in KEYWORDS:
+                token_type = 'keyword'
             else:
-                self.token_type = 'identifier'
+                token_type = 'identifier'
+            self.set_token(token_type, value)
+            self.position += (i - self.index)
             self.index = i
         else:
             raise ValueError(f"Invalid character {ch} at index {i}.")
@@ -124,7 +161,7 @@ class Tokeniser:
         """
         while self.has_next():
             self.advance()
-            yield (self.token_type, self.value)
+            yield self.token
 
 
 def main(args):
@@ -132,14 +169,9 @@ def main(args):
 
     try:
         with open(inpath, 'r') as fp:
-            print('<tokens>')
             tok = Tokeniser(fp)
-            for token_type, value in tok.generate():
-                value = str(value)
-                for esc, repl in ESCAPES:
-                    value = value.replace(esc, repl)
-                print(f'<{token_type}> {value} </{token_type}>')
-            print('</tokens>')
+            for token in tok.generate():
+                print(token)
         return 0
     except Exception as e:
         sys.stderr.write(str(e))
